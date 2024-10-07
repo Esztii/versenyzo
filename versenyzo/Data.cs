5f928743c01f97c8943cfef0069cc11bd173db49
orosz.eszter <orosz.eszter@28-DIAK7>
2024-10-07 10:39:57 +0200

    Add project files.

---
+++ versenyzo/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace versenyzo
+{
+    internal class Data
+    {
+        public string Nev { get; set; }
+        public int Szuletesev { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Sex { get; set; }
+        public string Kat { get; set; }
+        public Dictionary<string, TimeSpan> idok = new Dictionary<string, TimeSpan>();
+        public override string ToString()
+        {
+            return $"{Nev}, {Szuletesev}, {Rajtszam}, {(Sex? "f" : "n")}";
+        }
+
+        public Data(string beolvasottSor)
+        {
+            string[] d = beolvasottSor.Split(";");
+            Nev = d[0];
+            Szuletesev = int.Parse(d[1]);
+            Rajtszam = int.Parse(d[2]);
+            Sex = d[3] == "f";
+            Kat = d[4];
+            idok.Add("uszas", TimeSpan.Parse(d[5]));
+            idok.Add("elsodepo", TimeSpan.Parse(d[6]));
+            idok.Add("kerekpar", TimeSpan.Parse(d[7]));
+            idok.Add("masodikdepo", TimeSpan.Parse(d[8]));
+            idok.Add("futas", TimeSpan.Parse(d[9]));
+
+
+        }
+    }
+}
---
+++ versenyzo/Program.cs
@@ -0,0 +1,24 @@
+﻿using versenyzo;
+
+List<Data> datas = [];
+using StreamReader sr = new(@"..\..\..\src\forras.txt");
+while (!sr.EndOfStream) datas.Add(new(sr.ReadLine()!));
+
+var linqsum = datas.Count(d => d.Kat == "elit");
+Console.WriteLine($"Az elit-ek száma: {linqsum}");
+
+var linqavg = datas.Count(d => d.Sex = false);
+Console.WriteLine($"A női versenyzők átlagéletkora: {linqavg.}");
+
+var linqkerekpar = datas.Sum(d => d.idok[]);
+Console.WriteLine($"A versenyzők kerékpározással töltött összideje órában: ");
+
+var linquszas = datas.Average(d => d.idok);
+
+
+var feri = datas.
+
+
+
+
+
